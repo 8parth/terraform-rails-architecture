72c287c16f821dbb89680fa600b548582121d31d
Parth Modi <parthmodi54@yahoo.com>
2018-05-27 22:53:01 +0530

    userdata script updated

--- modules/web/files/user_data.sh
+++ modules/web/files/user_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+apt-get update -y
+apt-get install -y nginx > /var/nginx.log
 mkdir -p var/www/html
 cd var/www/html
 echo "Hello, World!" > index.html
-apt-get update -y
-apt-get install -y nginx > /var/nginx.log
