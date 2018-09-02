0975ed76eb3f48d61669ca28873765e74d9bf68c
Parth Modi <parthmodi54@yahoo.com>
2018-09-02 21:41:03 +0530

    fixed issue: when running `terraform apply` again private_internet_gateway routes failed
    nat_gateway_id used instead of gateway_id. (https://github.com/hashicorp/terraform/issues/9805)

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -98,7 +98,7 @@ resource "aws_route" "public_internet_gateway" {
 
 resource "aws_route" "private_internet_gateway" {
   route_table_id         = "${aws_route_table.private.id}"
-  gateway_id             = "${aws_nat_gateway.natgw.id}"
+  nat_gateway_id             = "${aws_nat_gateway.natgw.id}"
   destination_cidr_block = "0.0.0.0/0"
 }
 
